3c96709e2cfd6203142808306d26731397a1a805
somesheggy <sheggyAutomation@gmail.com>
2018-10-26 15:31:07 +0100

    Import RestAssured....

--- src/test/java/smokeTest.java
+++ src/test/java/smokeTest.java
@@ -1,9 +1,14 @@
 import org.junit.Test;
 
+import static io.restassured.RestAssured.expect;
+import static io.restassured.RestAssured.given;
+
 public class smokeTest {
 
     @Test
     public void startNow(){
+
+        System.out.println ("This is smoke Test Class");
 
 
     }
@@ -14,5 +19,24 @@ public class smokeTest {
 
     }
 
+
+
+    @Test
+    public void loginAndLogout(){
+        final String login = randomLogin();
+        // First post to login()
+        given()
+                .queryParam("login", login)
+                .queryParam("password", randomPassword())
+                .when().post("/login/");
+
+        // Second post to logout() with an assert
+        expect().statusCode(200)
+                .given()
+                .when().post("/logout/");
+    }
+
 }
+
+
 
